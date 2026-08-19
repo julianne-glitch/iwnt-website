1019d5f7654c57d005d8f92e385c8e6aff851db6
Julianne MOKOBO <julianneasanji6@gmail.com>
2026-08-19 14:18:42 +0100

    chore: add initial prisma migration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,52 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "Enquiry" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "workEmail" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "countryRegion" TEXT NOT NULL,
+    "helpTopic" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'NEW',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Enquiry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "topicId" TEXT,
+    "marketId" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "imageUrl" TEXT,
+    "titleEn" TEXT NOT NULL,
+    "titleFr" TEXT NOT NULL,
+    "contentEn" TEXT NOT NULL,
+    "contentFr" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AdminUser" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AdminUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Article_slug_key" ON "Article"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminUser_email_key" ON "AdminUser"("email");
+
